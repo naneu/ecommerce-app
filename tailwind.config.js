35ac773ce82c72c6068097ca33ab1bb380fbc1d1
naneu <nancysororo@.com>
2022-06-23 11:03:09 +0300

    retrive items from firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,8 +51,11 @@ module.exports = {
         primary: "#f5f3f3",
         cardOverlay: 'rgba(256,256,256,0.4)',
         lightTextGray: "#9ca0ab",
+        rowBg : 'rgba(255,131,0,0.05)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 };
